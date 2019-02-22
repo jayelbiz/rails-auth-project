631bf7bda55188884f11cf8891977ef74c853b0e
Jason <Jasonethedesigner@gmail.com>
2019-02-22 16:13:25 -0500

    Added static Pages Controllers

---
+++ app/controllers/status_pages_controller.rb
@@ -0,0 +1,7 @@
+class StatusPagesController < ApplicationController
+  def home
+  end
+
+  def help
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'status_pages/home'
+  get 'status_pages/help'
   resources :microposts
   resources :users
 
