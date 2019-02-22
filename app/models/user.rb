c3fa9e392609838404aec073c2877d8cfbe091a4
Jason <Jasonethedesigner@gmail.com>
2019-02-22 16:00:51 -0500

    user rb fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,9 @@ class User < ApplicationRecord
 
 
   has_many :microposts
-  validates FILL_IN, presence: true
-  validates FILL_IN, presence: true
+  validates :name, presence: true
+  validates :email, presence: true
+
 
 
 end
